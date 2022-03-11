68ab96103f2e90b3f128e4154966a40563bcdede
Jong Kim <jong.kim8706@gmail.com>
2022-03-12 05:39:47 +1100

    todo sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if(toDo === ""){
+      return;
+    }
+    console.log("submited")
+    setTodos(currentArray => [toDo, ...currentArray])
+    setTodo("")
+  }
+  const handleClear = () =>{
+    setTodos([])
+  }
+  const handleRemove = (index) => {
+    console.log("remove clicked")
+    setTodos(toDos.filter((item,toDoindex) => toDoindex !== index))
+    
+  }
+
+  return(
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit} >
+        <input value={toDo} onChange={onChange} type="text" placeholder='Wirte your to do...' />
+        <button type='submit'>Add To Do</button>
+        <button onClick={handleClear}>Clear</button>
+      </form>
+      <ul>
+        {toDos.map((item, index)=>{
+          return (<li key={index}>
+            {item} 
+            <button onClick={() => handleRemove(index)}>❌</button>
+            </li>
+          )
+      })}
+      </ul>
+      
+
     </div>
-  );
+  )
 }
 
 export default App;
